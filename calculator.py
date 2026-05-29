04f40267d28e509f22a8b9bdd9e9dea142293d1a
Eathan Davidson <superherosuperman1970@gmail.com>
2026-05-29 14:24:07 +0530

    Delete calculator.py

--- calculator.py
+++
@@ -1,8 +0,0 @@
-def add(a, b):
-    return a + b
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-result = add(num1, num2)
-print(f"Result: {num1} + {num2} = {result}")
